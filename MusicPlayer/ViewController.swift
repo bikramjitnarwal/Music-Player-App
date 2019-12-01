67f61a99d0d1ff6941e58133a0d271f7bd7df512
Bikramjit Narwal <bikramjit.narwal@mail.utoronto.ca>
2019-12-01 00:45:59 -0500

    First Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Bikramjit Narwal on 2019-12-01.
+//  Copyright © 2019 Bikramjit Narwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
